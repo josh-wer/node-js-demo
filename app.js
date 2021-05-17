eb8e3a5d5440e093a4dbe15ed7968e7e610732fa
Joshua Uyi <joshuaouyi@gmail.com>
2021-05-17 11:42:58 +0100

    added cart and orders

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('609d96eac6f02436dcd0b7e0')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
